5af635c9aa70a65f7f1364fe66b1b4786d7a1381
sunny <huiseon37@gmail.com>
2022-05-21 14:25:38 +0900

    chore: 파일 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Router from 'router';
+import Router from 'cores/router';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/globalStyle';
 import { theme } from 'styles/theme';
