34ed9d9de936231fd7d6e7885b2a44587b5f7319
aeuna <4294psy@naver.com>
2022-05-22 04:27:31 +0900

    feat: 결과화면 api 연결

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,13 +1,41 @@
 import BackGround from 'components/result/Background';
 import ResultMissionList from 'components/result/ResultMissionList';
-import React from 'react';
+import { client } from 'cores/api';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 function Result() {
+  const [headerInfo, setHeaderInfo] = useState({
+    date: '',
+    nickname: '',
+  });
+  const [missions, setMissions] = useState([]);
+  const { user } = useParams();
+
+  useEffect(() => {
+    getUserMissionInfo();
+  }, []);
+
+  const getUserMissionInfo = async () => {
+    const result = await client.get(`/mission/${user}/confirmed`);
+
+    let now = new Date();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+
+    setHeaderInfo({
+      date: `${month}월 ${day}일`,
+      nickname: result.data.data.user.nickname,
+    });
+
+    setMissions(result.data.data.missions);
+  };
+
   return (
     <StyledRoot>
       <BackGround />
-      <ResultMissionList />
+      <ResultMissionList headerInfo={headerInfo} missions={missions} />
     </StyledRoot>
   );
 }
